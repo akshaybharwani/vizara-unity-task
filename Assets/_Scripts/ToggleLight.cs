fddb93bb6d19172557cb86560d3bf2a0bd08ee6a
Akshay Bharwani <akshaybharwani@pm.me>
2020-07-11 15:54:37 +0530

    Add Toggle Light logic

---
+++ Assets/_Scripts/ToggleLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLight : MonoBehaviour {
+
+    public Light light;
+    private bool _on = false;
+
+    // Use this for initialization
+    void OnTriggerStay(Collider collider) {
+        // Checks for the Player, if the Key is Pressed
+        // and if the Light is on or not
+        if (collider.CompareTag("Player") && Input.GetKeyDown(KeyCode.E) && !_on)
+        {
+            // Turns Light on
+            light.enabled = true;
+            _on = true;
+        }
+        else if (collider.CompareTag("Player") && Input.GetKeyDown(KeyCode.E) && _on)
+        {
+            // Turns Light off
+            light.enabled = false;
+            _on = false;
+        }
+    }
+}
